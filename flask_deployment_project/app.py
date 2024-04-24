1513a49b79181122a1fa151019aa70498ea77d11
Tamilkumaran93 <156584667+Tamilkumaran93@users.noreply.github.com>
2024-04-24 15:58:41 +0530

    Add files via upload

---
+++ flask_deployment_project/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,render_template
+import pickle
+
+
+import numpy as np
+app=Flask(__name__)
+with open('Linear_regression_model.pkl','rb') as file:
+    model=pickle.load(file)
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    mid_sem_marks=request.form['MSE']
+    input_data=[[float(mid_sem_marks)]]
+    reshaped_data=np.array(input_data).reshape(1,-1)
+    prediction=model.predict(reshaped_data)
+    
+    return render_template('index.html',prediction=prediction[0])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
